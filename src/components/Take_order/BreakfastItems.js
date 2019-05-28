569a51d1a0781d51f430f539790af99b9142d961
TaphieM <taffymayen@gmail.com>
2019-05-28 17:07:54 -0500

    instalación de Enzyme para test, en el template OrderCheck se agrego setState para guardar el nombre de cliente, también se hizo el primer intento de imprimir los items sin exito

--- src/components/Take_order/BreakfastItems.js
+++ src/components/Take_order/BreakfastItems.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import Button from '../Button';
 import menu from '../../data/menu.json';
+import OrderCheck from './OrderCheck';
 
 class BreakfastItems extends React.Component {
   constructor(props) {
@@ -8,14 +10,14 @@ class BreakfastItems extends React.Component {
     this.addItem = this.addItem.bind(this);
   }
  
-  addItem() {
-    this.props.addtoOrder();
+  addItem(item) {
+    ReactDom.render(<OrderCheck  item={item} />, document.getElementById('confirmOrder'));
   }
 
   sandwiches() {
     const breakfastFood = menu.breakfastFood.map((Item) => {
       return (
-        <Button className="itembutton" name={Item.name} buttonOnClick={() => { this.addItem(Item) }} />
+        <Button className="itembutton" name={Item.name} options={() => { this.addItem(Item) }} />
       );
     });
     return breakfastFood;
@@ -24,7 +26,7 @@ class BreakfastItems extends React.Component {
   drinks() {
     const breakfastDrinks = menu.breakfastDrinks.map((Item) => {
       return (
-        <Button className="itembutton" name={Item.name} buttonOnClick={() => { this.addItem(Item) }} />
+        <Button className="itembutton" name={Item.name} options={() => { this.addItem(Item) }} />
       );
     });
     return breakfastDrinks;
--- src/components/Take_order/OrderCheck.js
+++ src/components/Take_order/OrderCheck.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
 class OrderCheck extends React.Component {
+  state = {
+    clientName: '', // Inicializamos el estado para que no salga error de cambio de controlado a no controlado
+  };
+
+  handleChange = e =>{
+    // console.log({
+    // name: e.target.name,
+    //   value: e.target.value,
+    // });  
+    this.setState({
+      [e.target.name]:e.target.value     // para que generalice el nombre donde guarda el valor
+    })
+  };
+  
+  handleClick = e => {
+    console.log("Button was clicked");
+    console.log(this.state);  
+  };
+
   render() {
     return (
       <element>
         <h2>Pedido</h2>
+        <div id="confirmOrder" />
         <form>
           <label htmlFor="name">
             Nombre
-            <input id="name" name="name" type="name" placeholder="Client Name" />
+            <input className="sendOrder" onChange={this.handleChange} value={this.state.clientName} id="name" name="clientName" type="text" placeholder="Client Name" />
           </label>
         </form>
-        <button type="button">Enviar a cocina</button>
+        <button onClick={this.handleClick} type="button">Enviar a cocina</button>
       </element>
     );
   }
 }
+
+
 
 export default OrderCheck;
